a664a540f0ebe243ddf07712abf422e43eb442db
[Ian Chao] <90526260+iantelli@users.noreply.github.com>
2022-11-21 00:56:48 -0800

    :lipstick: Updated embed messages

--- src/utils/embeds/beatmapPreview.ts
+++ src/utils/embeds/beatmapPreview.ts
@@ -1,40 +1,24 @@
 import { EmbedBuilder } from "@discordjs/builders"
+import { InteractionReplyOptions } from "discord.js"
+import { BeatmapResponseApi, BeatmapResponseApi as type } from "../../api"
 
-const beatmapPreviewEmbed = (
-    ParentSetID: number,
-    BeatmapID: number,
-    TotalLength: number,
-    HitLength: number,
-    DiffName: string,
-    FileMD5: string,
-    CS: number,
-    AR: number,
-    HP: number,
-    OD: number,
-    Mode: number,
-    BPM: number,
-    Playcount: number,
-    Passcount: number,
-    MaxCombo: number,
-    DifficultyRating: number
-): EmbedBuilder => {
+export const beatmapPreviewEmbed = (data: BeatmapResponseApi): InteractionReplyOptions => {
     const embed = new EmbedBuilder()
         .setColor(0xff8c00)
-        .setImage(`https://assets.ppy.sh/beatmaps/${ParentSetID}/covers/cover.jpg`)
-        .setTitle(DiffName)
-        .setDescription(`BeatmapID: ${BeatmapID}`)
-        .setURL(`https://osu.ppy.sh/beatmapsets/${ParentSetID}#osu/${BeatmapID}`)
+        .setImage(`https://assets.ppy.sh/beatmaps/${data.ParentSetID}/covers/cover.jpg`)
+        .setTitle(data.DiffName)
+        .setURL(`https://osu.ppy.sh/beatmapsets/${data.ParentSetID}#osu/${data.BeatmapID}`)
         .addFields(
-            { name: `⭐️`, value: `${DifficultyRating}`, inline: true },
-            { name: `⏱️`, value: `${TotalLength}`, inline: true },
-            { name: `BPM`, value: `${BPM}`, inline: true },
+            { name: `⭐️`, value: `${data.DifficultyRating}`, inline: true },
+            { name: `⏱️`, value: `${data.TotalLength}`, inline: true },
+            { name: `BPM`, value: `${data.BPM}`, inline: true },
             { name: "\u200B", value: "\u200B" },
-            { name: `CS`, value: `${CS}`, inline: true },
-            { name: `AR`, value: `${AR}`, inline: true },
-            { name: `HP`, value: `${HP}`, inline: true },
-            { name: `OD`, value: `${OD}`, inline: true }
+            { name: `CS`, value: `${data.CS}`, inline: true },
+            { name: `AR`, value: `${data.AR}`, inline: true },
+            { name: `HP`, value: `${data.HP}`, inline: true },
+            { name: `OD`, value: `${data.OD}`, inline: true }
         )
-    return embed
+    return {
+        embeds: [embed],
+    }
 }
-
-export default beatmapPreviewEmbed
--- src/utils/embeds/error.ts
+++ src/utils/embeds/error.ts
@@ -1,7 +1,10 @@
 import { EmbedBuilder } from "@discordjs/builders"
+import { InteractionReplyOptions } from "discord.js"
 
-const errorEmbed = (description: string): EmbedBuilder => {
-    return new EmbedBuilder().setColor(0xeb3434).setTitle("Something went wrong :(").setDescription(description)
+export const errorEmbed = (description: string): InteractionReplyOptions => {
+    const embed = new EmbedBuilder().setColor(0xeb3434).setTitle("Something went wrong :(").setDescription(description)
+    return {
+        embeds: [embed],
+        ephemeral: true,
+    }
 }
-
-export default errorEmbed
