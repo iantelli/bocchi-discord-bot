c9662f243fb82b6f946a45cf953ca8c56e171c63
Ian Chao <90526260+iantelli@users.noreply.github.com>
2022-11-21 14:06:48 -0800

    :pencil2: small change to footer

--- src/utils/embeds/beatmapPreview.ts
+++ src/utils/embeds/beatmapPreview.ts
@@ -1,7 +1,7 @@
-import { InteractionReplyOptions, ActionRowBuilder, ButtonBuilder, ButtonStyle, EmbedBuilder } from "discord.js"
+import { ActionRowBuilder, ButtonBuilder, ButtonStyle, EmbedBuilder, InteractionReplyOptions } from "discord.js"
 import { BeatmapResponseApi, BeatmapResponseApi as type } from "../../api"
-import { secondsToMinutes } from "../../utils"
 import keys from "../../keys"
+import { secondsToMinutes } from "../../utils"
 
 export const beatmapPreviewEmbed = (data: BeatmapResponseApi): InteractionReplyOptions => {
     const embed = new EmbedBuilder()
@@ -11,11 +11,10 @@ export const beatmapPreviewEmbed = (data: BeatmapResponseApi): InteractionReplyO
         .setURL(`https://osu.ppy.sh/beatmapsets/${data.ParentSetID}#osu/${data.BeatmapID}`)
         .addFields({
             name: `Beatmap Info`,
-            value: `${data.DifficultyRating}⭐️ - ${data.BPM}bpm - ${secondsToMinutes(data.TotalLength)}⏱️ - x/${
-                data.MaxCombo
-            }combo \n **CS**:${data.CS} **AR**:${data.AR} **HP**:${data.HP} **OD**:${data.OD}`,
+            value: `${data.DifficultyRating}⭐️ - ${data.BPM}bpm - ${secondsToMinutes(data.TotalLength)}⏱️ - x/${data.MaxCombo
+                }combo \n **CS**:${data.CS} **AR**:${data.AR} **HP**:${data.HP} **OD**:${data.OD}`,
         })
-        .setFooter({ text: `Bocchi Bot - Powered by Kitsu` })
+        .setFooter({ text: `Bocchi Bot - Powered by Kitsu API` })
 
     const row = new ActionRowBuilder<ButtonBuilder>().addComponents(
         new ButtonBuilder()
