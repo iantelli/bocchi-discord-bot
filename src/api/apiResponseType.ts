e671f7b11c9ecd91ec41cc41078410b18301dabf
[Ian Chao] <90526260+iantelli@users.noreply.github.com>
2022-11-20 23:22:46 -0800

    :label: Added beatmap response type

---
+++ src/api/apiResponseType.ts
@@ -0,0 +1,18 @@
+export interface BeatmapResponseApi {
+    ParentSetID: number
+    BeatmapID: number
+    TotalLength: number
+    HitLength: number
+    DiffName: string
+    FileMD5: string
+    CS: number
+    AR: number
+    HP: number
+    OD: number
+    Mode: number
+    BPM: number
+    Playcount: number
+    Passcount: number
+    MaxCombo: number
+    DifficultyRating: number
+}
