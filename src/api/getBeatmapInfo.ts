615f7c283aa476f7b24f131d423cced4ea711f7d
[Ian Chao] <90526260+iantelli@users.noreply.github.com>
2022-11-20 23:34:53 -0800

    :alien: Adding API call

---
+++ src/api/getBeatmapInfo.ts
@@ -0,0 +1,14 @@
+import { BeatmapResponseApi } from "./apiResponseType"
+import keys from "../keys"
+import axios from "axios"
+
+const getBeatmapInfo = async (beatmapId: string): Promise<BeatmapResponseApi> => {
+    try {
+        const response = await axios.get(keys.apiLink + "/b/" + beatmapId)
+        return response.data as BeatmapResponseApi
+    } catch (err) {
+        throw new Error("Beatmap not found")
+    }
+}
+
+export default getBeatmapInfo
