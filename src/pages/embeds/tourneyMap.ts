36f99c3a90da2257fb248f724051c31e04fb6cb8
[Ian Chao] <90526260+iantelli@users.noreply.github.com>
2022-11-25 09:50:07 -0800

    :bug: Fixed map length when DT mod is active

--- src/pages/embeds/tourneyMap.ts
+++ src/pages/embeds/tourneyMap.ts
@@ -1,22 +1,44 @@
 import { MapStats } from "@rian8337/osu-base"
-import { ActionRowBuilder, AttachmentBuilder, ButtonBuilder, ButtonStyle, EmbedBuilder, InteractionReplyOptions } from "discord.js"
+import {
+    ActionRowBuilder,
+    AttachmentBuilder,
+    ButtonBuilder,
+    ButtonStyle,
+    EmbedBuilder,
+    InteractionReplyOptions,
+} from "discord.js"
 import { BeatmapResponseApi } from "../../api"
 import keys from "../../keys"
 import { secondsToMinutes } from "../../utils"
 
-export const tourneyMapEmbed = (data: BeatmapResponseApi, pp: number, sheetId: string, mapName: string, mapStats: MapStats, starRating: number): InteractionReplyOptions => {
+export const tourneyMapEmbed = (
+    data: BeatmapResponseApi,
+    pp: number,
+    sheetId: string,
+    mapName: string,
+    mapStats: MapStats,
+    starRating: number
+): InteractionReplyOptions => {
     const file = new AttachmentBuilder(`./src/public/${sheetId}.png`)
     const embed = new EmbedBuilder()
-        .setColor(0xFF8EE6)
-        .setImage(`https://assets.ppy.sh/beatmaps/${data.ParentSetID}/covers/cover.jpg` ?? "https://assets.ppy.sh/beatmaps/355322/covers/cover.jpg")
+        .setColor(0xff8ee6)
+        .setImage(
+            `https://assets.ppy.sh/beatmaps/${data.ParentSetID}/covers/cover.jpg` ??
+                "https://assets.ppy.sh/beatmaps/355322/covers/cover.jpg"
+        )
         .setTitle(`${mapName} - ${data.DiffName}`)
         .setThumbnail(`attachment://${sheetId}.png`)
         .setDescription(`**${pp.toFixed(2)}** pp`)
         .setURL(`https://osu.ppy.sh/beatmapsets/${data.ParentSetID}#osu/${data.BeatmapID}`)
         .addFields({
             name: `Beatmap Info`,
-            value: `${starRating.toFixed(2)}⭐️ - ${data.BPM * mapStats.speedMultiplier}bpm - ${sheetId === "DT" ? secondsToMinutes(data.TotalLength * 0.66) : secondsToMinutes(data.TotalLength)}⏱️ - x/${data.MaxCombo
-                } combo \n CS ${mapStats.cs!.toFixed(1)} | AR ${mapStats.ar!.toFixed(1)} | OD ${mapStats.od!.toFixed(1)} | HP ${mapStats.hp!.toFixed(1)}`,
+            value: `${starRating.toFixed(2)}⭐️ - ${data.BPM * mapStats.speedMultiplier}bpm - ${
+                sheetId.slice(0, -1) === "DT"
+                    ? secondsToMinutes(data.TotalLength * 0.66)
+                    : secondsToMinutes(data.TotalLength)
+            }⏱️ - x/${data.MaxCombo} combo \n CS ${mapStats.cs!.toFixed(1)} | AR ${mapStats.ar!.toFixed(
+                1
+            )} | OD ${mapStats.od!.toFixed(1)} | HP ${mapStats.hp!.toFixed(1)}`,
         })
         .setFooter({ text: `Bocchi Bot - Powered by Kitsu API` })
         .setTimestamp(new Date())
